cef31ce8300e66b4a7472277a643d265def31f26
Merlinov <71987887+lmerlinoff@users.noreply.github.com>
2023-04-18 00:18:18 +0300

    Update MusicPlayer.cs

--- MusicPlayer.cs
+++ MusicPlayer.cs
@@ -1,52 +1,46 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.IO;
+using System.Collections;
 using System.Collections.Generic;
-using NAudio.Wave;
-
+using System.IO;
+using System.Linq;
 
 public class MusicPlayer : MonoBehaviour
 {
+    public AudioSource audioSource;
     public Text songTitle;
     public Slider volumeSlider;
     public Button playButton;
     public Button nextButton;
     public Button prevButton;
-    public Button updateButton;
-    public AudioSource audioSource;
 
-    private List<string> musicList = new List<string>();
-    private int currentSongIndex = -1;
+    private List<string> songPaths;
+    private int currentSongIndex = 0;
 
     void Start()
     {
-        // Create the "My music" folder if it doesn't exist
-        if (!Directory.Exists(Application.persistentDataPath + "/My music"))
+        string[] musicExtensions = new string[] { ".wav", ".mp3" };
+        string musicFolderPath = Application.dataPath + "/My Music/";
+        songPaths = new List<string>();
+
+        foreach (string file in Directory.GetFiles(musicFolderPath))
         {
-            Directory.CreateDirectory(Application.persistentDataPath + "/My music");
+            if (musicExtensions.Contains(Path.GetExtension(file)))
+            {
+                songPaths.Add(file);
+            }
         }
 
-        // Find all music files in the "My music" folder
-        string[] filePaths = Directory.GetFiles(Application.persistentDataPath + "/My music", "*.mp3");
+        audioSource.clip = GetAudioClip(songPaths[currentSongIndex]);
+        audioSource.Play();
 
-        // Add the music files to the list
-        foreach (string filePath in filePaths)
-        {
-            musicList.Add(filePath);
-        }
-
-        // Set the initial volume and play the first song in the list
-        audioSource.volume = volumeSlider.value;
-        PlaySong(0);
+        playButton.onClick.AddListener(TogglePlayPause);
+        nextButton.onClick.AddListener(NextSong);
+        prevButton.onClick.AddListener(PrevSong);
+        volumeSlider.onValueChanged.AddListener(ChangeVolume);
     }
 
-    void Update()
-    {
-        // Update the volume as the slider changes
-        audioSource.volume = volumeSlider.value;
-    }
-
-    public void PlayPause()
+    private void TogglePlayPause()
     {
         if (audioSource.isPlaying)
         {
@@ -60,49 +54,31 @@ public class MusicPlayer : MonoBehaviour
         }
     }
 
-    public void NextSong()
+    private void NextSong()
     {
-        if (currentSongIndex < musicList.Count - 1)
-        {
-            currentSongIndex++;
-            PlaySong(currentSongIndex);
-        }
-    }
-
-    public void PrevSong()
-    {
-        if (currentSongIndex > 0)
-        {
-            currentSongIndex--;
-            PlaySong(currentSongIndex);
-        }
-    }
-
-    public void UpdateMusic()
-    {
-        // Clear the current music list
-        musicList.Clear();
-
-        // Find all music files in the "My music" folder
-        string[] filePaths = Directory.GetFiles(Application.persistentDataPath + "/My music", "*.mp3");
-
-        // Add the music files to the list
-        foreach (string filePath in filePaths)
-        {
-            musicList.Add(filePath);
-        }
-    }
-
-    private void PlaySong(int index)
-    {
-        // Stop the current song and update the song title
-        audioSource.Stop();
-        currentSongIndex = index;
-        songTitle.text = Path.GetFileNameWithoutExtension(musicList[currentSongIndex]);
-
-        // Play the new song
-        audioSource.clip = NAudioPlayer.FromMp3Data(File.ReadAllBytes(musicList[currentSongIndex]));
+        currentSongIndex = (currentSongIndex + 1) % songPaths.Count;
+        audioSource.clip = GetAudioClip(songPaths[currentSongIndex]);
         audioSource.Play();
-        playButton.GetComponentInChildren<Text>().text = "Pause";
+        songTitle.text = Path.GetFileNameWithoutExtension(songPaths[currentSongIndex]);
+    }
+
+    private void PrevSong()
+    {
+        currentSongIndex = (currentSongIndex - 1 + songPaths.Count) % songPaths.Count;
+        audioSource.clip = GetAudioClip(songPaths[currentSongIndex]);
+        audioSource.Play();
+        songTitle.text = Path.GetFileNameWithoutExtension(songPaths[currentSongIndex]);
+    }
+
+    private void ChangeVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+
+    private AudioClip GetAudioClip(string path)
+    {
+        WWW www = new WWW("file://" + path);
+        while (!www.isDone) { }
+        return www.GetAudioClip(false, false);
     }
 }
